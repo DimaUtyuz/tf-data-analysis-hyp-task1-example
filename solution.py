c4d7bebcd6ea0889d4c8ef84e19936cacfd32763
DimaUtyuz <dima-utyuzhnikov2013@yandex.ru>
2023-04-12 00:10:13 +0300

    added solution

--- solution.py
+++ solution.py
@@ -1,14 +1,11 @@
-import pandas as pd
-import numpy as np
+import statsmodels.stats.proportion as p
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 617910360 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    _, pval = p.proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+    return pval <= 0.1
