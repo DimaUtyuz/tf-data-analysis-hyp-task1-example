f74ea2629a4105196360d00bf2b41da538f3f455
DimaUtyuz <dima-utyuzhnikov2013@yandex.ru>
2023-04-12 00:34:40 +0300

    fixed solution

--- solution.py
+++ solution.py
@@ -1,4 +1,7 @@
+import numpy
 import statsmodels.stats.proportion as p
+from scipy.stats import chi2
+from scipy.stats import laplace, norm
 
 
 chat_id = 617910360 # Ваш chat ID, не меняйте название переменной
@@ -7,5 +10,8 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    _, pval = p.proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
-    return pval <= 0.1
+    v = (x_success / x_cnt - y_success / y_cnt) * numpy.sqrt((x_cnt + y_cnt) * x_cnt * y_cnt / ((x_success + y_success) * (x_cnt + y_cnt - x_success - y_success)))
+    q = norm.ppf(0.1)
+    return v <= q
+
+# print(solution(2, 1000, 50, 1000))
